c205968bda6388cfe350757c9733848a79d9fb37
Bernardo Vivens Moreno <bernardo.vivens@nbn23.tech>
2022-04-11 09:19:04 +0200

    First commit

---
+++ go.mod
@@ -0,0 +1,41 @@
+module github.com/NBN23dev/go-secret-manager
+
+go 1.18
+
+require (
+	cloud.google.com/go/secretmanager v1.4.0
+	github.com/googleapis/gax-go v1.0.3
+	github.com/stretchr/testify v1.7.1
+	golang.org/x/oauth2 v0.0.0-20220309155454-6242fa91716a
+	google.golang.org/api v0.74.0
+	google.golang.org/genproto v0.0.0-20220407144326-9054f6ed7bac
+)
+
+require (
+	cloud.google.com/go v0.100.2 // indirect
+	cloud.google.com/go/compute v1.5.0 // indirect
+	cloud.google.com/go/iam v0.3.0 // indirect
+	github.com/BurntSushi/toml v1.1.0 // indirect
+	github.com/davecgh/go-spew v1.1.0 // indirect
+	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/google/go-cmp v0.5.7 // indirect
+	github.com/googleapis/gax-go/v2 v2.2.0 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/stretchr/objx v0.1.0 // indirect
+	go.opencensus.io v0.23.0 // indirect
+	golang.org/x/exp v0.0.0-20220407100705-7b9b53b0aca4 // indirect
+	golang.org/x/exp/typeparams v0.0.0-20220407100705-7b9b53b0aca4 // indirect
+	golang.org/x/lint v0.0.0-20210508222113-6edffad5e616 // indirect
+	golang.org/x/mod v0.6.0-dev.0.20220106191415-9b9b3d81d5e3 // indirect
+	golang.org/x/net v0.0.0-20220407224826-aac1ed45d8e3 // indirect
+	golang.org/x/sys v0.0.0-20220406163625-3f8b81556e12 // indirect
+	golang.org/x/text v0.3.7 // indirect
+	golang.org/x/tools v0.1.11-0.20220316014157-77aa08bb151a // indirect
+	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
+	google.golang.org/appengine v1.6.7 // indirect
+	google.golang.org/grpc v1.45.0 // indirect
+	google.golang.org/protobuf v1.28.0 // indirect
+	gopkg.in/yaml.v3 v3.0.0-20200313102051-9f266ea9e77c // indirect
+	honnef.co/go/tools v0.3.0 // indirect
+)
---
+++ library.go
@@ -0,0 +1,105 @@
+package secret_manager
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"strings"
+
+	secretmanager "cloud.google.com/go/secretmanager/apiv1"
+	secretmanagerpb "google.golang.org/genproto/googleapis/cloud/secretmanager/v1"
+
+	"github.com/googleapis/gax-go"
+	"golang.org/x/oauth2/google"
+	"google.golang.org/api/compute/v1"
+)
+
+type client interface {
+	AccessSecretVersion(
+		ctx context.Context,
+		req *secretmanagerpb.AccessSecretVersionRequest,
+		opts ...gax.CallOption,
+	) (*secretmanagerpb.AccessSecretVersionResponse, error)
+	Close() error
+}
+
+type SecretManager struct {
+	client  client
+	project string
+	cache   map[string]string
+}
+
+// NewSecretManager creates a new SecretManager instance to acess secrets.
+func NewSecretManager() (*SecretManager, error) {
+	ctx := context.Background()
+
+	client, err := secretmanager.NewClient(ctx)
+
+	if err != nil {
+		return nil, err
+	}
+
+	credentials, err := google.FindDefaultCredentials(ctx, compute.ComputeReadonlyScope)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &SecretManager{
+		client:  client,
+		project: credentials.ProjectID,
+		cache:   map[string]string{},
+	}, nil
+}
+
+// AccessSecrets gets the secrets from environment values.
+// It obtains the secret of each environment value if it has his value equal to `@secret`.
+func (sm *SecretManager) AccessSecrets() error {
+	for _, element := range os.Environ() {
+		env := strings.Split(element, "=")
+
+		key := env[0]
+		val := env[1]
+
+		if val == "@secret" {
+			data, _ := sm.AccessSecret(key)
+
+			err := os.Setenv(key, data)
+
+			if err != nil {
+				return err
+			}
+		}
+	}
+
+	return nil
+}
+
+// AccessSecret get a secret value from a name.
+func (sm *SecretManager) AccessSecret(name string) (string, error) {
+	if value, ok := sm.cache[name]; ok {
+		return value, nil
+	}
+
+	sname := fmt.Sprintf("projects/%s/secrets/%s/versions/latest", sm.project, name)
+
+	ctx := context.Background()
+	vr := &secretmanagerpb.AccessSecretVersionRequest{Name: sname}
+
+	secret, err := sm.client.AccessSecretVersion(ctx, vr)
+
+	if err != nil {
+		return "", err
+	}
+
+	value := string(secret.Payload.Data[:])
+
+	sm.cache[name] = value
+
+	return value, nil
+}
+
+// Close
+func (sm *SecretManager) Close() error {
+	return sm.client.Close()
+}
